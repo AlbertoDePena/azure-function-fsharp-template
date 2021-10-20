3c0e4e126f1112238edaa8a128f2f57754f5f928
Alberto De Pena <albertodepena89@gmail.com>
2021-10-19 21:21:58 -0500

    Added more extensions methods

--- Extensions.fs
+++ Extensions.fs
@@ -2,10 +2,25 @@ namespace azure_function_fsharp
 
 [<AutoOpen>]
 module HttpRequestExtensions =
+    open System
     open Microsoft.AspNetCore.Http
     open Newtonsoft.Json
 
     type HttpRequest with
+
+        member this.TryGetBearerToken() =
+            this.Headers
+            |> Seq.tryFind (fun q -> q.Key = "Authorization")
+            |> Option.map (fun q -> if Seq.isEmpty q.Value then String.Empty else q.Value |> Seq.head)
+            |> Option.map (fun h -> h.Substring("Bearer ".Length).Trim())
+
+        member this.TryGetQueryStringValue(name: string) =
+            let hasValue, values = this.Query.TryGetValue(name)
+            if hasValue then values |> Seq.tryHead else None
+
+        member this.TryGetHeaderValue(name: string) =
+            let hasHeader, values = this.Headers.TryGetValue(name)
+            if hasHeader then values |> Seq.tryHead else None
 
         member this.TryGetFormValue(key: string) =
             match this.HasFormContentType with
