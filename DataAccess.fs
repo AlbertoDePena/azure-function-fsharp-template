13cedb7cde270f09e80617b1c1875654a799c1fb
Alberto De Pena <albertodepena89@gmail.com>
2023-06-01 15:36:04 -0500

    Refactoring

--- DataAccess.fs
+++ DataAccess.fs
@@ -8,11 +8,11 @@ module DbConnection =
     open Microsoft.Data.SqlClient
 
     /// <exception cref="System.ArgumentException"></exception>
-    let create (connectionString: string) =
-        if String.IsNullOrWhiteSpace connectionString then
+    let create (dbConnectionString: string) =
+        if String.IsNullOrWhiteSpace dbConnectionString then
             invalidArg "dbConnectionString" "Database connection string cannot be null or empty"
         else
-            new SqlConnection(connectionString) :> IDbConnection
+            new SqlConnection(dbConnectionString) :> IDbConnection
 
 [<RequireQualifiedAccess>]
 module OptionTypeHandler =
