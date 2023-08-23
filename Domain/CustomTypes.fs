f71f728698ec6e55e2ab9abf2a2cc06e3eb1e7d4
Alberto De Pena <alberto.depena@craneww.com>
2023-08-23 07:27:46 -0500

    Using class for constraint types

--- Domain/CustomTypes.fs
+++ Domain/CustomTypes.fs
@@ -1,14 +1,14 @@
 namespace azure_function_fsharp.Domain.CustomTypes
 
 open System
-open System.Text.RegularExpressions
 
 [<RequireQualifiedAccess>]
 module private Singleton =
+    open System.Text.RegularExpressions
 
-    let private emailRegex = lazy(new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+    let private lazyEmailRegex = lazy(new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
 
-    let EmailRegex = emailRegex.Force()
+    let EmailRegex = lazyEmailRegex.Force()
 
 type EmailAddress = 
     private 
@@ -25,98 +25,92 @@ type EmailAddress =
             Error (sprintf "%s is not a valid email address" value)
     
 /// Positive whole number including zero
-type NaturalNumber = private NaturalNumber of int
+type NaturalNumber = 
+    private 
+    | NaturalNumber of int
 
-[<RequireQualifiedAccess>]
-module NaturalNumber =
-    
-    let isValid (x: int) = x >= 0
+    member this.Value = 
+        match this with
+        | NaturalNumber value -> value
 
-    let value (NaturalNumber x) = x
-
-    let tryCreate (x: int) =
-        if isValid x then
-            x |> NaturalNumber |> Some
+    static member TryCreate (value: int) =
+        if value >= 0 then
+            value |> NaturalNumber |> Ok
         else
-            None
+            Error (sprintf "%i is not a natural number" value)
 
 /// Positive decimal number including zero
-type NaturalDecimalNumber = private NaturalDecimalNumber of decimal
+type NaturalDecimalNumber = 
+    private 
+    | NaturalDecimalNumber of decimal
 
-[<RequireQualifiedAccess>]
-module NaturalDecimalNumber =
-    
-    let isValid (x: decimal) =  x >= 0.0M
+    member this.Value = 
+        match this with
+        | NaturalDecimalNumber value -> value
 
-    let value (NaturalDecimalNumber x) = x
-
-    let tryCreate (x: decimal) =
-        if isValid x then
-            x |> NaturalDecimalNumber |> Some
+    static member TryCreate (value: decimal) =
+        if value >= 0.0M then
+            value |> NaturalDecimalNumber |> Ok
         else
-            None            
+            Error (sprintf "%f is not a natural decimal number" value)
 
 /// Positive whole number excluding zero
-type PositiveNumber = private PositiveNumber of int            
+type PositiveNumber = 
+    private 
+    | PositiveNumber of int            
 
-[<RequireQualifiedAccess>]
-module PositiveNumber =
-    
-    let isValid (x: int) = x > 0
+    member this.Value = 
+        match this with
+        | PositiveNumber value -> value
 
-    let value (PositiveNumber x) = x
-
-    let tryCreate (x: int) =
-        if isValid x then
-            x |> PositiveNumber |> Some
+    static member TryCreate (value: int) =
+        if value > 0 then
+            value |> PositiveNumber |> Ok
         else
-            None
+            Error (sprintf "%i is not a positive number" value)
 
 /// Decimal number excluding zero
-type PositiveDecimalNumber = private PositiveDecimalNumber of decimal               
+type PositiveDecimalNumber = 
+    private 
+    | PositiveDecimalNumber of decimal               
 
-[<RequireQualifiedAccess>]
-module PositiveDecimalNumber =
-    
-    let isValid (x: decimal) = x > 0M
+    member this.Value = 
+        match this with
+        | PositiveDecimalNumber value -> value
 
-    let value (PositiveDecimalNumber x) = x
-
-    let tryCreate (x: decimal) =
-        if isValid x then
-            x |> PositiveDecimalNumber |> Some
+    static member TryCreate (value: decimal) =
+        if value > 0M then
+            value |> PositiveDecimalNumber |> Ok
         else
-            None
+            Error (sprintf "%f is not a positive decimal number" value)
 
-type Text = private Text of string
+type NonEmptyText = 
+    private 
+    | NonEmptyText of string
 
-[<RequireQualifiedAccess>]
-module Text =
+    member this.Value = 
+        match this with
+        | NonEmptyText value -> value
 
-    let isValid (x: string) = String.IsNullOrWhiteSpace x |> not
-
-    let value (Text x) = x
-
-    let tryCreate (x: string) =
-        if isValid x then
-            x.Trim() |> Text |> Some
+    static member TryCreate (value: string) =
+        if String.IsNullOrWhiteSpace value then
+            Error "The text is required and cannot be empty"
         else
-            None
+            value.Trim() |> NonEmptyText |> Ok
+            
+type UniqueId = 
+    private 
+    | UniqueId of Guid
 
-type UniqueId = private UniqueId of Guid
+    member this.Value = 
+        match this with
+        | UniqueId value -> value
 
-[<RequireQualifiedAccess>]
-module UniqueId =
-    open RT.Comb
-
-    let isValid (x: Guid) = x <> Guid.Empty
-
-    let value (UniqueId x) = x
-
-    let create () = Provider.Sql.Create() |> UniqueId
-
-    let tryCreate (x: Guid) =
-        if isValid x then
-            x |> UniqueId |> Some
+    static member TryCreate (value: Guid) =
+        if value <> Guid.Empty then
+            value |> UniqueId |> Ok
         else
-            None
+            Error (sprintf "%O is not a valid unique identifier" value)
+
+    static member Create () = 
+        RT.Comb.Provider.Sql.Create() |> UniqueId
